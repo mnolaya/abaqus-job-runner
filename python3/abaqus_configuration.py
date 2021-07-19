063e024329be1d410cd9cc4cf13186eda924f27c
Michael Olaya <michael.n.olaya@gmail.com>
2021-07-19 14:36:46 -0400

    Quick commit.

--- python3/abaqus_configuration.py
+++ python3/abaqus_configuration.py
@@ -15,7 +15,7 @@ class configure_abaqus:
     max_recents = 10
 
     def __init__(self):
-        self.config = configparser.ConfigParser()
+        pass
 
     def build_job(self, inpname, subname, jobname=None, delete_mode='ON'):
         # Create job name from inp file name if none is provided.
@@ -32,55 +32,28 @@ class configure_abaqus:
         fmtstr = job_dict['cmd'].format(job=jobname, inp=inpname, sub=subname, delete=delete_mode)
         return fmtstr
 
-    def save_selected_jobs(self, job_list):
-        now = date.today()
-        if type(job_list) is not list: job_list = [job_list]
-        section = 'RECENT-JOBS'
-        config = self.config
-        config.read(self.ini_file)
-
-        # Check number of existing options in recent jobs section.
-        if config.has_section(section): 
-            recents = config.options(section)
-            num_recents = len(recents)
-            if num_recents == self.max_recents:
-                num_recents = num_recents - 1        
-        else:
-            recents = []
-            num_recents = 0
-            config[section] = {}
-
-        # Create temp option for recents section to be populated by job command(s).
-        fmtstr = 'recent {num}'.format(num=(num_recents + 1))
-        config[section][fmtstr] = 'temp'
-
-        # Insert list of commands to beginning of recents list, then update config.
-        old_jobs = [config[section][r] for r in recents]
-        old_jobs.insert(0, job_list)
-        for i, r in enumerate(config[section]):
-            store_dict =  {
-                'cmds': old_jobs[i],
-                'date': now.strftime('%Y-%m-%d'),
-                'time': now.strftime('%H:%M%S'),
-            }
-            config[section][r] = str(store_dict)
-
-        # Write updated config file.     
-        with open(self.ini_file, 'w') as f:
-            config.write(f)
+    def read_json(self, json_file=json_file):
+        with open(json_file, 'r') as f:
+            config = json.load(f)
+        return config
+    
+    def write_json(self, config, json_file=json_file):
+        with open(json_file, 'w') as f:
+            json.dump(config, f, indent=4)
 
     def read_recents(self):
         section = 'RECENT-JOBS'
-        config = self.config
-        config.read(self.ini_file)
-
+        config = self.read_json()
         for i, r in enumerate(config[section]):
-            cmd = config[section][r]
-            print(cmd)
-            # fmtstr = '\n-----\nKey: {key}\nKey: {key}\nKey: {key}\nCommand(s): {cmd}\n-----'
-            # print(fmtstr.format(key=i, cmd=cmd))
+            recent = config[section][r]
+            cmd = recent['cmd']
+            date = recent['date']
+            time = recent['time']
+            fmtstr = '-----\nKey: {key}\nDate: {date} | Time: {time}\nCommand(s): {cmd}\n-----'
+            print(fmtstr.format(key=i, date=date, time=time, cmd=cmd))
 
-    def json_save_selected_jobs(self, job_list):
+    def save_selected_jobs(self, job_list):
+        # Create json if one doesn't already exit.
         section = 'RECENT-JOBS'
         if not os.path.isfile(self.json_file):
             recents = {
@@ -88,16 +61,13 @@ class configure_abaqus:
                         'Recent 1': {},
                 },
             }
-            with open(self.json_file, 'w') as f:
-                json.dump(recents, f, indent=4)
+            self.write_json(recents)
 
-        now = datetime.today()
-        if type(job_list) is not list: job_list = [job_list]
-
-        with open(self.json_file, 'r') as f:
-            config = json.load(f)
+        # Ensure input job command(s) are in list form.       
+        if type(job_list) is not list: job_list = [job_list]       
 
         # Check number of existing options in recent jobs section.
+        config = self.read_json()
         if section in config:
             recents = [r for r in config[section]]
             num_recents = len(recents)
@@ -113,8 +83,9 @@ class configure_abaqus:
 
         # Insert list of commands to beginning of recents list, then update config.
         old_jobs = [config[section][r] for r in recents]
+        now = datetime.today()
         store_dict =  {
-            'cmds': job_list,
+            'cmd': job_list,
             'date': now.strftime('%Y-%m-%d'),
             'time': now.strftime('%H:%M:%S'),
         }
@@ -122,11 +93,8 @@ class configure_abaqus:
         for i, r in enumerate(config[section]):
             config[section][r] = old_jobs[i]
 
-        print(config)
-
         # Write updated config file.     
-        with open(self.json_file, 'w') as f:
-            json.dump(config, f, indent=4)
+        self.write_json(config)
 
 # Testing framework.
 if __name__ == "__main__":
@@ -149,4 +117,5 @@ if __name__ == "__main__":
         cmd = config_aba.build_job(inpname=f, subname=subname)
         new_recent.append(cmd)
 
-    config_aba.json_save_selected_jobs(new_recent)
+    config_aba.save_selected_jobs(new_recent)
+    config_aba.read_recents()
